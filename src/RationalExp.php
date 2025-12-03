3243d569d54da1206ef6ff21a7648940a96b28f0
Sergey N. Kruk <scruoge@gmail.com>
2025-12-03 22:54:26 +0300

    initial commit

---
+++ src/RationalExp.php
@@ -0,0 +1,203 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Scruoge\Fractions;
+
+use DivisionByZeroError;
+use JsonSerializable;
+
+final readonly class RationalExp implements JsonSerializable
+{
+    private function __construct(
+        private int $significand,
+        private int $denominator = 1,
+        private int $exponent = 0,
+    ) {
+    }
+
+    public function mul(self $that): self
+    {
+        return self::normalize(
+            $this->significand * $that->significand,
+            $this->denominator * $that->denominator,
+            $this->exponent + $that->exponent,
+        );
+    }
+
+    public function div(self $that): self
+    {
+        return self::normalize(
+            $this->significand * $that->denominator,
+            $this->denominator * $that->significand,
+            $this->exponent - $that->exponent,
+        );
+    }
+
+    public function add(self $that): self
+    {
+        $exponentDiff = $that->exponent - $this->exponent;
+        if ($exponentDiff === 0) {
+            return self::normalize(
+                $this->significand * $that->denominator + $that->significand * $this->denominator,
+                $this->denominator * $that->denominator,
+                $this->exponent,
+            );
+        }
+        if ($exponentDiff > 0) {
+            return self::normalize(
+                $this->significand * $that->denominator + $that->significand * 10 ** $exponentDiff * $this->denominator,
+                $this->denominator * $that->denominator,
+                $this->exponent,
+            );
+        }
+
+        return self::normalize(
+            $this->significand * 10 ** -$exponentDiff * $that->denominator + $that->significand * $this->denominator,
+            $this->denominator * $that->denominator,
+            $that->exponent,
+        );
+    }
+
+    public function sub(self $that): self
+    {
+        return $this->add(new self(-$that->significand, $that->denominator, $that->exponent));
+    }
+
+    public function equals(self $that): bool
+    {
+        $d = $this->sub($that);
+        return $d->significand === 0 && $d->denominator === 1;
+    }
+
+    public function toFloat(): float
+    {
+        return $this->significand / $this->denominator * 10 ** $this->exponent;
+    }
+
+    /**
+     * @return int[]
+     */
+    public function jsonSerialize(): array
+    {
+        return [
+            'significand' => $this->significand,
+            'denominator' => $this->denominator,
+            'exponent' => $this->exponent,
+        ];
+    }
+
+    public static function create(
+        int $significand,
+        int $exponent = 0,
+        int $denominator = 1,
+    ): self
+    {
+        if ($denominator === 0) {
+            throw new DivisionByZeroError('Denominator cannot be zero.');
+        }
+
+        return self::normalize($significand, $denominator, $exponent);
+    }
+
+    public static function fromNumber(
+        int|float $number,
+        int $digits = -1,
+        int $roundMode = PHP_ROUND_HALF_UP,
+    ): self
+    {
+        if (is_int($number)) {
+            return self::normalize($number, 1, 0);
+        }
+
+        if ($digits >= 0) {
+            return self::normalize(
+                significand: (int) round($number * 10 ** $digits, mode: $roundMode),
+                denominator: 1,
+                exponent: -$digits,
+            );
+        }
+
+        $significand = $number;
+        $exponent = 0;
+        while ($significand - (int) $significand !== 0.0) {
+            $significand *= 10;
+            $exponent--;
+        }
+
+        return self::normalize(
+            significand: (int) $significand,
+            denominator: 1,
+            exponent: $exponent
+        );
+    }
+
+    public static function zero(): self
+    {
+        return new self(
+            significand: 0,
+            exponent: 0
+        );
+    }
+
+    public static function one(): self
+    {
+        return new self(
+            significand: 1,
+            exponent: 0
+        );
+    }
+
+    private static function gcd(int $a, int $b): int
+    {
+        if ($a === $b) {
+            return $a;
+        }
+        if ($a === 0 || $b === 0) {
+            return 0;
+        }
+        if ($a === 1 || $b === 1) {
+            return 1;
+        }
+        if (!($a & 1 || $b & 1)) {
+            return self::gcd($a >> 1, $b >> 1) << 1;
+        }
+        if (!($a & 1)) {
+            return self::gcd($a >> 1, $b);
+        }
+        if (!($b & 1)) {
+            return self::gcd($a, $b >> 1);
+        }
+        if ($a > $b) {
+            return self::gcd(($a - $b) >> 1, $b);
+        }
+        return self::gcd(($b - $a) >> 1, $a);
+    }
+
+    static private function normalize(
+        int $significand,
+        int $denominator,
+        int $exponent
+    ): self
+    {
+        if ($significand === 0) {
+            return new self($significand);
+        }
+
+        $gcd = self::gcd(abs($significand), abs($denominator));
+        $significand = intdiv($significand, $gcd);
+        $denominator = intdiv($denominator, $gcd);
+
+        while ($significand - ($ts = (int)($significand * .1)) * 10 === 0) {
+            $significand = $ts;
+            $exponent++;
+        }
+
+        while ($denominator - ($td = (int)($denominator * .1)) * 10 === 0) {
+            $denominator = $td;
+            $exponent--;
+        }
+
+        return new self($significand, $denominator, $exponent);
+    }
+}
---
+++ test/RationalExpTest.php
@@ -0,0 +1,234 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Scruoge\Fractions\Test;
+
+use DivisionByZeroError;
+use PHPUnit\Framework\TestCase;
+use ReflectionClass;
+use ReflectionException;
+use Scruoge\Fractions\RationalExp;
+
+final class RationalExpTest extends TestCase
+{
+    /**
+     * @throws ReflectionException
+     */
+    public function testConstructors(): void
+    {
+        $actual = RationalExp::zero();
+        $expected = RationalExp::create(0);
+        self::assertObjectEquals($expected, $actual, 'equals', sprintf(
+            'Failed to assert that %0.3f equals to %0.3f',
+            $actual->toFloat(),
+            $expected->toFloat()
+        ));
+        $actual = RationalExp::one();
+        $expected = RationalExp::create(10, -1);
+        self::assertObjectEquals($expected, $actual, 'equals', sprintf(
+            'Failed to assert that %0.3f equals to %0.3f',
+            $actual->toFloat(),
+            $expected->toFloat()
+        ));
+
+        $actual = RationalExp::fromNumber(123);
+        $expected = RationalExp::create(123);
+        self::assertObjectEquals($expected, $actual, 'equals', sprintf(
+            'Failed to assert that %0.3f equals to %0.3f',
+            $actual->toFloat(),
+            $expected->toFloat()
+        ));
+
+        $actual = RationalExp::fromNumber(1234.2345);
+        $expected = RationalExp::create(12342345, -4);
+        self::assertObjectEquals($expected, $actual, 'equals', sprintf(
+            'Failed to assert that %0.3f equals to %0.3f',
+            $actual->toFloat(),
+            $expected->toFloat()
+        ));
+
+        $actual = RationalExp::fromNumber(1234.2345, 3);
+        $expected = RationalExp::create(1234235, -3);
+        self::assertObjectEquals($expected, $actual, 'equals', sprintf(
+            'Failed to assert that %0.3f equals to %0.3f',
+            $actual->toFloat(),
+            $expected->toFloat()
+        ));
+
+        $actual = RationalExp::fromNumber(1234.2345, 3, PHP_ROUND_HALF_DOWN);
+        $expected = RationalExp::create(1234234, -3);
+        self::assertObjectEquals($expected, $actual, 'equals', sprintf(
+            'Failed to assert that %0.3f equals to %0.3f',
+            $actual->toFloat(),
+            $expected->toFloat()
+        ));
+
+        $actual = RationalExp::fromNumber(1234.2345, 0);
+        $expected = RationalExp::create(1234);
+        self::assertObjectEquals($expected, $actual, 'equals', sprintf(
+            'Failed to assert that %0.3f equals to %0.3f',
+            $actual->toFloat(),
+            $expected->toFloat()
+        ));
+
+        $a = RationalExp::create(12, 0, 4);
+        $reflection = new ReflectionClass(RationalExp::class);
+        $reflection->getMethod('gcd')->invoke($a, 0, 1);
+
+        self::expectException(DivisionByZeroError::class);
+        RationalExp::create(1, 0, 0);
+    }
+
+    public function testSerializers()
+    {
+        $a = RationalExp::create(1,2,3);
+        self::assertEquals(
+            [
+                'significand' => 1,
+                'denominator' => 3,
+                'exponent' => 2,
+            ],
+            $a->jsonSerialize(),
+        );
+    }
+
+    public function testAdd(): void
+    {
+        $a = RationalExp::create(230, -2);
+        $b = RationalExp::create(120, -2);
+        $c = RationalExp::one();
+        $actual = $a->add($b);
+        $expected = RationalExp::create(350, -2);
+        self::assertObjectEquals($expected, $actual, 'equals', sprintf(
+            'Failed to assert that %0.3f equals to %0.3f',
+            $actual->toFloat(),
+            $expected->toFloat()
+        ));
+        $actual = $a->add($c);
+        $expected = RationalExp::create(330, -2);
+        self::assertObjectEquals($expected, $actual, 'equals', sprintf(
+            'Failed to assert that %0.3f equals to %0.3f',
+            $actual->toFloat(),
+            $expected->toFloat()
+        ));
+        $a = RationalExp::create(3, 0, 8);
+        $b = RationalExp::create(1, 0, 3);
+        $actual = $a->add($b);
+        $expected = RationalExp::create(17, 0, 24);
+        self::assertObjectEquals($expected, $actual, 'equals', sprintf(
+            'Failed to assert that %0.3f equals to %0.3f',
+            $actual->toFloat(),
+            $expected->toFloat()
+        ));
+        $a = RationalExp::create(7, 1, 15);
+        $b = RationalExp::create(5, 0, 6);
+        $actual = $a->add($b);
+        $expected = RationalExp::create(11, 0, 2);
+        self::assertObjectEquals($expected, $actual, 'equals', sprintf(
+            'Failed to assert that %0.3f equals to %0.3f',
+            $actual->toFloat(),
+            $expected->toFloat()
+        ));
+    }
+
+    public function testSub(): void
+    {
+        $a = RationalExp::create(230, -2);
+        $b = RationalExp::create(120, -2);
+        $c = RationalExp::one();
+        $actual = $a->sub($b);
+        $expected = RationalExp::create(110, -2);
+        self::assertObjectEquals($expected, $actual, 'equals', sprintf(
+            'Failed to assert that %0.3f equals to %0.3f',
+            $actual->toFloat(),
+            $expected->toFloat()
+        ));
+        $actual = $b->sub($a);
+        $expected = RationalExp::create(-110, -2);
+        self::assertObjectEquals($expected, $actual, 'equals', sprintf(
+            'Failed to assert that %0.3f equals to %0.3f',
+            $actual->toFloat(),
+            $expected->toFloat()
+        ));
+        $actual = $b->sub($c);
+        $expected = RationalExp::create(20, -2);
+        self::assertObjectEquals($expected, $actual, 'equals', sprintf(
+            'Failed to assert that %0.3f equals to %0.3f',
+            $actual->toFloat(),
+            $expected->toFloat()
+        ));
+        $a = RationalExp::create(1, 0, 3);
+        $b = RationalExp::create(3, 0, 8);
+        $actual = $a->sub($b);
+        $expected = RationalExp::create(-1, 0, 24);
+        self::assertObjectEquals($expected, $actual, 'equals', sprintf(
+            'Failed to assert that %0.3f equals to %0.3f',
+            $actual->toFloat(),
+            $expected->toFloat()
+        ));
+        $a = RationalExp::create(7, 0, 15);
+        $b = RationalExp::create(5, 0, 6);
+        $actual = $a->sub($b);
+        $expected = RationalExp::create(-11, 0, 30);
+        self::assertObjectEquals($expected, $actual, 'equals', sprintf(
+            'Failed to assert that %0.3f equals to %0.3f',
+            $actual->toFloat(),
+            $expected->toFloat()
+        ));
+    }
+
+    public function testMul(): void
+    {
+        $a = RationalExp::create(230, -2);
+        $b = RationalExp::create(120, -2);
+        $actual = $a->mul($b);
+        $expected = RationalExp::create(12 * 23, -2);
+        self::assertObjectEquals($expected, $actual, 'equals', sprintf(
+            'Failed to assert that %0.3f equals to %0.3f',
+            $actual->toFloat(),
+            $expected->toFloat()
+        ));
+        $b = RationalExp::create(1, 0, 3);
+        $a = RationalExp::create(3, 0, 8);
+        $actual = $a->mul($b);
+        $expected = RationalExp::create(3, 0, 24);
+        self::assertObjectEquals($expected, $actual, 'equals', sprintf(
+            'Failed to assert that %0.3f equals to %0.3f',
+            $actual->toFloat(),
+            $expected->toFloat()
+        ));
+        $a = RationalExp::create(7, 0, 15);
+        $b = RationalExp::create(5, 0, 6);
+        $actual = $a->mul($b);
+        $expected = RationalExp::create(7, 0, 18);
+        self::assertObjectEquals($expected, $actual, 'equals', sprintf(
+            'Failed to assert that %0.3f equals to %0.3f',
+            $actual->toFloat(),
+            $expected->toFloat()
+        ));
+    }
+
+    public function testDiv(): void
+    {
+        $a = RationalExp::create(230, -2);
+        $b = RationalExp::create(120, -2);
+        $actual = $a->div($b);
+        $expected = RationalExp::create(23, 0, 12);
+        self::assertObjectEquals($expected, $actual, 'equals', sprintf(
+            'Failed to assert that %0.3f equals to %0.3f',
+            $actual->toFloat(),
+            $expected->toFloat()
+        ));
+
+        $a = RationalExp::create(330, -2);
+        $b = RationalExp::create(110, -3);
+        $actual = $a->div($b);
+        $expected = RationalExp::create(30);
+        self::assertObjectEquals($expected, $actual, 'equals', sprintf(
+            'Failed to assert that %0.3f equals to %0.3f',
+            $actual->toFloat(),
+            $expected->toFloat()
+        ));
+    }
+}
